4dc9205c0504c46dc63ecdf31b4aaa3369edde55
Badr <badreddine.elasraoui@ump.ac.ma>
2025-08-14 10:34:25 +0200

    fix multiple isuues + addd subnets and its variables

--- main.tf
+++ main.tf
@@ -1,25 +1,44 @@
 ## EC2 Instances ---------------------------------------
-resource "aws_instance" "front_instance" {
+resource "aws_instance" "bei_front_instance" {
   ami                    = var.ami
   instance_type          = var.instance_type
-  subnet_id              = aws_subnet.private_subnet.id
+  subnet_id              = aws_subnet.bei_public_subnet.id
 
   tags = {
-    Name = "front_instance"
+    Name = "bei_front_instance"
   }
 }
 
-resource "aws_instance" "back_instance" {
+resource "aws_instance" "bei_back_instance" {
   ami                    = var.ami
   instance_type          = var.instance_type
-  subnet_id              = aws_subnet.private_subnet.id
+  subnet_id              = aws_subnet.bei_private_subnet.id
 
   tags = {
-    Name = "back_instance"
+    Name = "bei_back_instance"
   }
 }
 
 ## VPC -----------------------------------------
 data "aws_vpc" "main_vpc" {
    id = var.vpc_id
+}
+
+## Subnets -----------------------------
+resource "aws_subnet" "bei_public_subnet" {
+  vpc_id                  = data.aws_vpc.main_vpc.id
+  cidr_block              = var.public_subnet_cidr
+
+  tags = {
+    Name = "bei_public-subnet"
+  }
+}
+
+resource "aws_subnet" "bei_private_subnet" {
+  vpc_id                  = data.aws_vpc.main_vpc.id
+  cidr_block              = var.private_subnet_cidr
+
+  tags = {
+    Name = "bei_private-subnet"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,19 @@
 variable "ami" {
-  type = "string"
+  type = string
 }
 
 variable "instance_type" {
-  type = "string"
+  type = string
 }
 
 variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "private_subnet_cidr" {
   type = string
 }
