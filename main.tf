382952060a18610473998813af5dbd97c25aa218
Badr <badreddine.elasraoui@ump.ac.ma>
2025-08-14 12:46:23 +0200

    add egress in sg, install angular in front ec2

--- main.tf
+++ main.tf
@@ -1,21 +1,30 @@
 ## EC2 Instances ---------------------------------------
 resource "aws_instance" "bei_front_instance" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.bei_public_subnet.id
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.bei_public_subnet.id
   associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.front_sg.id]
+  user_data_replace_on_change = true
 
-  user_data = <<-EOF
-              sudo yum update -y
-              curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
-              sudo yum install -y nodejs
-              sudo npm install -g @angular/cli
-              EOF
+  user_data = <<EOF
+#!/bin/bash
+set -euxo pipefail
+export DEBIAN_FRONTEND=noninteractive
 
-  tags = {
-    Name = "bei_front_instance"
-  }
+apt-get update -y
+apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt-get install -y nodejs build-essential
+npm install -g @angular/cli http-server
+echo 'export badr1=5' >> /home/ubuntu/.bashrc
+
+
+EOF
+
+  tags = { Name = "bei_front_instance" }
 }
+
 
 resource "aws_instance" "bei_back_instance" {
   ami                    = var.ami
@@ -90,4 +99,29 @@ resource "aws_route_table" "private_rt" {
 resource "aws_route_table_association" "private_assoc" {
   subnet_id      = aws_subnet.bei_private_subnet.id
   route_table_id = aws_route_table.private_rt.id
+}
+
+## Security Groups ---------------------------------
+resource "aws_security_group" "front_sg" {
+  name        = "front-instance-sg"
+  description = "Security group for front instance"
+  vpc_id      = data.aws_vpc.main_vpc.id
+
+  ingress {
+    description = "SSH from any IP (testing purposes)"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "front_sg"
+  }
 }
