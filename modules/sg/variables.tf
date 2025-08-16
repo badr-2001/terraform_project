1e21841ca8242400d16e69958bfd92b932d6960d
Badr <badreddine.elasraoui@ump.ac.ma>
2025-08-17 01:33:08 +0200

    add sg module

---
+++ modules/sg/variables.tf
@@ -0,0 +1,34 @@
+variable "vpc_id" {
+  type        = string
+}
+
+variable "sg_name" {
+  type        = string
+}
+
+variable "sg_description" {
+  type        = string
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
+
+variable "egress_rules" {
+  description = "List of egress rules"
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = []
+}
