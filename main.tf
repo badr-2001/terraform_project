6db7e282288684a9c9777c3d2a509d849aceb786
Badr <badreddine.elasraoui@ump.ac.ma>
2025-08-18 10:30:39 +0200

    create 2 left subnets

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "bei_public_subnet_1" {
   source = "./modules/subnet"
   vpc_id = module.vpc.id
   cidr_block = "172.31.10.0/28"
-  availability_zone = "us-west-2a"
+  availability_zone = "eu-west-1a"
   subnet_name="bei_public_subnet_1"
   map_public_ip_on_launch = true
 }
@@ -17,8 +17,27 @@ module "bei_private_subnet_1" {
   source = "./modules/subnet"
   vpc_id = module.vpc.id
   cidr_block = "172.31.10.32/28"
-  availability_zone = "us-west-2a"
+  availability_zone = "eu-west-1a"
   subnet_name="bei_private_subnet_1"
 }
+module "bei_public_subnet_2" {
+  source = "./modules/subnet"
+  vpc_id = module.vpc.id
+  cidr_block = "172.31.10.16/28"
+  availability_zone = "eu-west-1b"
+  subnet_name="bei_public_subnet_2"
+  map_public_ip_on_launch = true
+}
 
-  
+module "bei_private_subnet_2" {
+  source = "./modules/subnet"
+  vpc_id = module.vpc.id
+  cidr_block = "172.31.10.48/28"
+  availability_zone = "eu-west-1b"
+  subnet_name="bei_private_subnet_2"
+}
+
+module "backend" {
+  source      = "./modules/backend"
+  bucket_name = "bei2-bucket"
+}  
