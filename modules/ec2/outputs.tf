b9e6fe37e33b97fb428ee53f71ff89c6ad8d8b3c
Badr <badreddine.elasraoui@ump.ac.ma>
2025-08-19 09:36:03 +0200

    add bucket encryption and versioning

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,4 +1,4 @@
 output "id" {
   value = aws_instance.this.id
 }
-output "public_ip" { value = try(aws_instance.this.public_ip, null) }
+output "public_ip" { value = aws_instance.this.public_ip}
--- modules/igw/outputs.tf
+++ modules/igw/outputs.tf
@@ -1,3 +1,5 @@
 output "id" {
   value = var.create_igw ? aws_internet_gateway.this[0].id : data.aws_internet_gateway.this[0].id
 }
+
+##When we use count in Terraform, the resource becomes a list of objects, and each element corresponds to one instance of that resource
