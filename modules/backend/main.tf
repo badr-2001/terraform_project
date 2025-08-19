8d88d7b6111bad7f52be31ffcedca65fb9b0d39e
Badr <badreddine.elasraoui@ump.ac.ma>
2025-08-19 09:37:33 +0200

    add bucket encryption and versioning

--- modules/backend/main.tf
+++ modules/backend/main.tf
@@ -12,3 +12,23 @@ resource "aws_s3_bucket" "tfstate" {
     #type = "S"
   #}
 #}
+
+# Enable versioning
+resource "aws_s3_bucket_versioning" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable default AES-256 encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
